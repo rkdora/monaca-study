ca3080b83fcdfb4257f7cdf683913674f3b66403
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2019-05-25 14:12:15 +0900

    gannbatta

---
+++ www/js/DataBase.js
@@ -0,0 +1,150 @@
+function DataBase(){
+    this.pickLanguage();
+};
+
+DataBase.prototype = {
+    init: function() {
+        
+    },
+    
+    pick: function(fieldName,targetName){
+        var Data = ncmb.DataStore("Data");
+        Data.order(fieldName,false)
+            .equalTo(filedName,targetName)
+            .fetchAll()
+            .then(function(results){
+
+
+                
+            })
+            .catch(function(err){
+                alert(err);
+            });
+    },
+    pickLanguage: async function() {
+        var Data = ncmb.DataStore("Data");
+        var obj = [];
+        console.log("1");
+        await Data.order("Language",false)
+            .fetchAll()
+            .then(function(results){
+                var sorted = [];
+                var k = 1;
+                console.log(results);
+                var buff = results[0].get("Language");
+                sorted[0] = buff;
+                console.log("2");
+                for(i = 1; i < results.length; i++){
+                    
+                    if(results[i].get("Language") != buff){
+                        sorted[k] = results[i].get("Language");
+                        k++;
+                        buff = results[i].get("Language");   
+                    }
+                }
+                console.log("3");
+
+                for(i = 0; i < sorted.length; i++){
+                    console.log("sorted:"+sorted[i]);
+                    
+                    var target = document.getElementById("defaultOption")
+                    var option = document.createElement('option');
+                    option.label = sorted[i];
+                    option.value = sorted[i]; 
+                    target.insertAdjacentElement("afterend", option);
+                }
+                
+            })
+            .catch(function(err){
+                alert(err);
+            });
+        
+    },
+
+    extract: function(array,fieldName) {
+        return array.get(fieldName);
+    },    
+
+    save: function(user,language,element) {
+        var Data = ncmb.DataStore("Data");
+        var data = new Data();
+
+        data.set("User",user)
+            .set("Language",language)
+            .set("Element",element)
+            .save()
+            .then(function(results){
+                alert("saved");
+            })
+            .catch(function(err){
+                alert("save err");
+            });
+    },
+    showCheckBox: function(){
+        var inputValue = getId("language").value;
+        //console.log(inputValue);
+        if(inputValue == "other"){
+            var target = getId("language")
+            var langText = document.createElement('input');
+            var langButton = document.createElement('input');
+            var elementText = document.createElement('input');
+            var elementButton = document.createElement('input');
+            elementText.type = "text";
+            elementButton.type = "button";
+            elementButton.id = "newLanguageButton";
+            elementButton.value = "要素登録";
+            target.insertAdjacentElement("afterend", elementButton);
+            target.insertAdjacentElement("afterend", elementText);
+            langText.type = "text";
+            langButton.type = "button";
+            langButton.id = "newLanguageButton";
+            langButton.value = "言語登録";
+            target.insertAdjacentElement("afterend", langButton);
+            target.insertAdjacentElement("afterend", langText);
+            
+        }
+        else{
+            var Data = ncmb.DataStore("Data");
+          //  console.log(inputValue);
+            Data.order("Language",false)
+                    .equalTo("Language",inputValue)
+                    .fetchAll()
+                    .then(function(results){
+                        var elesorted = [];
+                        var k = 1;
+                        console.log("results[0]"+results[0].get("Element"));
+                        var buff = results[0].get("Element");
+                      //  console.log(buff);
+                        elesorted[0] = buff;
+                        
+                        for(i = 1; i < results.length; i++){
+                            
+                            if(results[i].get("Element") != buff){
+                                elesorted[k] = results[i].get("Element");
+                                k++;
+                                buff = results[i].get("Element");
+                              //  console.log(buff);
+                            }
+                        }
+                        for(i = 0; i < elesorted.length; i++){
+                            console.log(elesorted[i]);
+                            var target = document.getElementById("language");
+                            var checkBox = document.createElement('input');
+                            checkBox.type = "checkbox";
+                            checkBox.value = elesorted[i];
+                            target.insertAdjacentHTML("afterend", elesorted[i]);
+                            target.insertAdjacentElement("afterend", checkBox);
+                           
+                        }
+                        
+                    })
+                    .catch(function(err){
+                        alert(err);
+                    });
+            }
+            
+
+    }
+    
+       
+};
---
+++ www/js/prochart.js
@@ -0,0 +1,35 @@
+function pon(text){
+    alert(text);
+};
+function log(text){
+    console.log(text);
+};
+
+function appear(){
+    var div=document.getElementById("record");
+    div.setAttribute("style","display:inline");
+};
+
+function genTextBox(TARGET, POSITION){
+    var target = document.getElementById(TARGET);
+    var textBox = document.createElement('input');
+    var resisterBox = document.createElement('input');
+    
+    textBox.type = "text";
+    textBox.id = "NewLangName"
+    resisterBox.type = "button";
+    resisterBox.id = "newLangName";
+    resisterBox.value = "完了";
+    resisterBox.onclick = function() {
+        ln.save();
+    };
+    
+    target.insertAdjacentElement(POSITION, resisterBox);
+    target.insertAdjacentElement(POSITION, textBox);
+    
+    console.log("create form");
+};
+
+function getId(targetId){
+    return window.document.getElementById(targetId);
+};
